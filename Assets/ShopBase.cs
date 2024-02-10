9384f203ee4d77bacc51ec847d32b657e0f0a98a
SaitoF3110 <134474025+SaitoF3110@users.noreply.github.com>
2024-02-10 10:15:03 +0900

    ショップの購入システム 1.15を購入回数で累乗した分必要リソースが増えます。 UnityEventでResouceManagerの関数を呼び出しています。

--- Assets/ShopBase.cs
+++ Assets/ShopBase.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 /// <summary>施設の基底クラス</summary>
 public class ShopBase : MonoBehaviour
 {
+    [Serializable] public class OnClickEvent : UnityEvent<float> { }
+
+    [Header("メソッドを選ぶ時、上の方にある【Dynamic】の方を呼ぶこと！！")]
+    [SerializeField] OnClickEvent _purchaseEvent = null;
+
+    //public UnityEvent _purchaseEvent;
+    [Tooltip("購入アイテムの種類")]
+    [SerializeField] Item _item;
+    [Tooltip("購入アイテムの値段（初期値）")]
+    [SerializeField] int _price;
+
     /// <summary>購入した施設の所持数</summary>
-    int _level = 0; 
+    int _level = 0;
+
+    //以下は開発用
+    [SerializeField] Text _text;
     void Start()
     {
         
     }
-    void Update()
+    private void Update()
     {
-        
+        _text.text = "現在施設レベル" + _level +"　：必要リソース" + (Math.Pow(1.15, _level) * _price).ToString("F0");
     }
     /// <summary>購入した施設やアップグレードの処理を上書きしてください</summary>
     public virtual void ItemProcess()
@@ -20,10 +37,12 @@ public class ShopBase : MonoBehaviour
 
     }
     /// <summary>購入処理。Buttonから呼び出す</summary>
-    public void Purchase(Item item)
+    public void Purchase()
     {
-        //ここにリソース消費処理を作る予定
-        if (item == Item.Upgrade)//もしアップグレードだったら
+        //リソース消費処理
+        _purchaseEvent.Invoke((float)(Math.Pow(1.15, _level) * _price));
+
+        if (_item == Item.Upgrade)//もしアップグレードだったら
         {
             //ここにUIを消す処理
         }
