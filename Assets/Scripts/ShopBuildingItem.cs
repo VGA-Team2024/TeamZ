9af5170463cfb7ed536acc4f3472c21f430843b5
Enulu <enuarle8@icloud.com>
2024-02-11 21:08:01 +0900

    saitoさんのShopBaseにあわせてShopBuildingItemを改良しました。 ShopUpgradeItemに毎秒加算する総量を2%増やす機能を追加しました。 UpgradeItemを1度だけ使用できるようにしました。

--- Assets/Scripts/ShopBuildingItem.cs
+++ Assets/Scripts/ShopBuildingItem.cs
@@ -1,45 +1,52 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ShopBuildingItem : ShopBase
 {
-    /// <summary>
-    /// 注意：このクラスはショップのBuildingアイテムButtonに張り付けるつもりで書きました変更点やご不満がある方は野田倫太郎までお申し付けください。
-    /// </summary>
+    // 注意：このクラスはショップの施設のアイテムに張り付けるつもりで書きました変更点やご不満がある方は野田倫太郎までお申し付けください。
 
+    ///<summary>resourseの増加数</summary>
     [SerializeField] public float resoursePlus = default;
-    //resourseの増加数
-    [SerializeField] float shopCost = default;
-    //shopにあるItemのコスト
-    [SerializeField] float CostPlus = default;
-    //Costの増加倍率
+    /// <summary>ResourceManagerがついてるObjectを取得する</summary>
+    [Header("ReaourceManagerが入ったObjectを入れてください")]
     [SerializeField] GameObject resourceManagerObject = null;
-    //ResourceManagerがついてるObjectを取得する
-    ResourceManager resourceManager;
-    //ResourceManagerの関数を取得するための入れ物
+    /// <summary>"この施設によって"1秒間に生成されるResourceの数</summary>
+    [Header("Publicだけどいじらないで")]public float everySecondPlusResource = default;
 
     void Start()
     {
-        resourceManager = resourceManagerObject.GetComponent<ResourceManager>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        _resouce = resourceManagerObject.GetComponent<ResourceManager>();
+        //ResourceManagerにアクセス
     }
 
     public void OnClickBuildingButton (string Building)
     {
-        if(resourceManager.ResourceTotalAmount >= shopCost)
+        bool firstClick = true;
+        if (_resouce.ResourceTotalAmount >= _price && firstClick == true)
         {
-            resourceManager.UseResource(shopCost);
-            Purchase();
-            shopCost *= CostPlus;
-            resourceManager.AddEverySecond(resoursePlus);
+            _item = Item.Repeat;//itemの判別
+            Purchase();//購入処理
+            _resouce.AddEverySecond(resoursePlus);//ItemによるResourceの増加処理
+            everySecondPlusResource += resoursePlus;//このItemによる毎秒のResource増加数
+            firstClick = false;
+        }//最初は初期値とリソースの総量を比較する
+
+        if (_resouce.ResourceTotalAmount >= _nowPrice && firstClick == false)
+        {
+            _item = Item.Repeat;//itemの判別
+            Purchase();//購入処理
+            _resouce.AddEverySecond(resoursePlus);//ItemによるResourceの増加処理
+            everySecondPlusResource += resoursePlus;//このItemによる毎秒のResource増加数
         }
+
         else
         {
             Debug.Log("コストが足りません");
         }
     }
+
+    public void Upgrade(float x)
+    {
+        _resouce.AddEverySecond(everySecondPlusResource);
+        everySecondPlusResource *= x;
+    }//アップグレードアイテムを使ったときの処理
 }
--- Assets/Scripts/ShopUpgradeItem.cs
+++ Assets/Scripts/ShopUpgradeItem.cs
@@ -1,27 +1,54 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ShopUpgradeItem : ShopBase
 {
-    /// <summary>
-    /// 注意：このクラスはショップのUpgradeアイテムButtonにくっつけるつもりで書きました変更点やご不満がある方は野田倫太郎までお申し付けください。
-    /// </summary>
+    // 注意：このクラスはショップのUpgradeアイテムにくっつけるつもりで書きました変更点やご不満がある方は野田倫太郎までお申し付けください。
 
-    [SerializeField] GameObject ShopBuildingItemObject = null;
-    //Shopにあるアップグレードさせたいアイテムを入れる
+    /// <summary>Shopにあるアップグレードさせたいアイテムを入れる</summary>
+    [Header("アップグレードさせたいObjectを入れてください(Resource全体の増加量を増やしたい場合はnullにしてください）")]
+    [SerializeField] GameObject shopBuildingItemObject = null;
+    /// <summary>ShopBuildingItemObjectのアイテムをどれくらいアップデートさせるか</summary>
+    [Header("施設のアップグレード倍率")]
+    [SerializeField] float upgradePowerBuilding = default;
+    [Header("Resource全体のアップグレード倍率")]
+    [SerializeField] float upgradePowerTotalAmount = default;
     ShopBuildingItem shopBuildingItem;
     void Start()
     {
-        shopBuildingItem = ShopBuildingItemObject.GetComponent<ShopBuildingItem>();
+        if (shopBuildingItemObject != null)
+        {
+            shopBuildingItem = shopBuildingItemObject.GetComponent<ShopBuildingItem>();
+        }
     }
     
     void Update()
     {
-        
+        if(shopBuildingItemObject == null)
+        {
+            upgradePowerTotalAmount = ((float)_resouce.ResourceTotalAmount/100) * 2;
+        } //全体の2%の数を取得
     }
 
     public void OnClickUpgradeButton(string Upgrade)
     {
-        shopBuildingItem.resoursePlus *= 2;
-        Purchase();
+        if (shopBuildingItemObject != null)
+        {
+            _item = Item.Upgrade;//アイテムの判別
+            Purchase();//アイテムの支払い
+            if (_resouce.ResourceTotalAmount >= _price)
+            {
+                shopBuildingItem.Upgrade(upgradePowerBuilding);//施設のアップグレード
+                this.gameObject.SetActive(false);//1度使用したら使えないようにする
+            }
+            else
+            {
+                Debug.Log("Resourceが足りないよ");
+            }
+        }//shopBuildingItemObjectに施設のアイテムが入っていればその施設のアップグレードを行う
+        else
+        {
+            _resouce.AddEverySecond(upgradePowerTotalAmount);
+            this.gameObject.SetActive(false);//1度使用したら使えないようにする
+        }//shopBuildingItemObjectに何も入っていなければ毎秒のResource増加数を全体の２％増やす
     }
 }
