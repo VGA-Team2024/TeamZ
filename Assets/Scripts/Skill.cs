f013277066f0603d49b917ceb550af4f6dbd2797
yuyakohashi <134474229+yuyakohashi@users.noreply.github.com>
2024-02-21 17:23:23 +0900

    変数名を変更しました

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Skill : NpcBase
+{
+    ///<summary>resourseの増加数</summary>
+    [SerializeField] public float goldPlus = default;
+    /// <summary>ResourceManagerがついてるObjectを取得する</summary>
+    [Header("GoldManagerが入ったObjectを入れてください")]
+    [SerializeField] GameObject goldManagerObject = null;
+    /// <summary>"この施設によって"1秒間に生成されるResourceの数</summary>
+    [Header("Publicだけどいじらないで")]public float everySecondPlusGold = default;
+
+    void Start()
+    {
+        _gold = goldManagerObject.GetComponent<GoldManager>();
+        //GoldManagerにアクセス
+    }
+
+    public void OnClickSkillButton (string Building)
+    {
+        bool firstClick = true;
+        if (_gold.GoldTotalAmount >= _price && firstClick == true)
+        {
+            _npc = Npc.Repeat;//NPCの判別
+            Purchase();//購入処理
+            _gold.AddEverySecond(goldPlus);//ItemによるGoldの増加処理
+            everySecondPlusGold += goldPlus;//このItemによる毎秒のGold増加数
+            firstClick = false;
+        }//最初は初期値とゴールドの総量を比較する
+
+        if (_gold.GoldTotalAmount >= _nowPrice && firstClick == false)
+        {
+            _npc = Npc.Repeat;//NPCの判別
+            Purchase();//購入処理
+            _gold.AddEverySecond(goldPlus);//ItemによるGoldの増加処理
+            everySecondPlusGold += goldPlus;//このItemによる毎秒のGold増加数
+        }
+
+        else
+        {
+            Debug.Log("コストが足りません");
+        }
+    }
+
+    public void Upgrade(float x)
+    {
+        _gold.AddEverySecond(everySecondPlusGold);
+        everySecondPlusGold *= x;
+    }//アップグレードアイテムを使ったときの処理
+}
